d756456fabb96c27a866ca63ea3ed0de7a12c2de
MhamediSirine <156354773+MhamediSirine@users.noreply.github.com>
2025-01-01 12:21:45 +0100

    init

---
+++ app/src/main/java/centre/elife/fronted_autoconfiance/MainActivity.kt
@@ -0,0 +1,47 @@
+package centre.elife.fronted_autoconfiance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import centre.elife.fronted_autoconfiance.ui.theme.Fronted_AutoConfianceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Fronted_AutoConfianceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Fronted_AutoConfianceTheme {
+        Greeting("Android")
+    }
+}
