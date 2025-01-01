f3747d0695fbdc705d6cb5d910dfe16b0c043183
MhamediSirine <156354773+MhamediSirine@users.noreply.github.com>
2025-01-01 12:46:35 +0100

    gradle

--- app/src/main/java/centre/elife/fronted_autoconfiance/MainActivity.kt
+++ app/src/main/java/centre/elife/fronted_autoconfiance/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
@@ -42,6 +43,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Fronted_AutoConfianceTheme {
-        Greeting("Android")
+        Greeting("sirine")
     }
 }
