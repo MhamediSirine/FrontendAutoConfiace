ee586ecdfdb819ac688aa5eb4a2e4043658b52df
hacker4world <zonegaming514@gmail.com>
2025-01-23 12:16:16 +0000

    integrated with update employee profile

---
+++ app/src/main/java/centre/elife/fronted_autoconfiance/Repositories/EmployeeRepository.kt
@@ -0,0 +1,20 @@
+package centre.elife.fronted_autoconfiance.Repositories
+
+import centre.elife.fronted_autoconfiance.data.Dto.UpdateEmployeeAccountDto
+import centre.elife.fronted_autoconfiance.data.models.UpdateProfileResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.PUT
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
+
+interface EmployeeRepository {
+    @PUT("/api/employee/update-account")
+    suspend fun updateEmployeeAccount(
+        @Header("Authorization") token: String,
+        @Body employeeData: UpdateEmployeeAccountDto
+    ): Response<UpdateProfileResponse>
+}
---
+++ app/src/main/java/centre/elife/fronted_autoconfiance/data/Dto/UpdateEmployeeAccountDto.kt
@@ -0,0 +1,7 @@
+package centre.elife.fronted_autoconfiance.data.Dto
+
+data class UpdateEmployeeAccountDto(val email: String,
+                                    val name: String,
+                                    val lastName: String,
+                                    val address: String,
+                                    val birthDate: String,)
